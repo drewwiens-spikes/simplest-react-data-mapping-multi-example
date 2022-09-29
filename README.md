9b024de494ab5134003e12209d78b495640d0cf8
Drew Wiens <6962078+drewwiens@users.noreply.github.com>
2022-09-29 16:29:33 -0500

    Removes unnecessary parts

---
+++ README.md
@@ -0,0 +1,4 @@
+### The react-data-mapping example without the unimportant parts
+
+https://www.npmjs.com/package/react-data-mapping
+https://github.com/aliyun/data-mapping-component
--- index.jsx
+++ index.jsx
@@ -2,216 +2,200 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Layout, Row, Col, Button } from "antd";
-import * as _ from "lodash";
 import DataMapping from "react-data-mapping";
-import * as SingleNoHeaderData from "./mock_data/single-no-header";
-import * as SingleWithHeaderData from "./mock_data/single-with-header";
-import * as MutiplyMappingData from "./mock_data/mutiply-mapping";
-import * as SinglePointLimit from "./mock_data/single-point-limit";
-import * as DiffColumns from "./mock_data/diff-columns";
 
-import "antd/dist/antd.css";
 import "react-data-mapping/dist/index.css";
-// import "react-data-mapping/dist/static/butterfly.css";
-// import "react-data-mapping/dist/static/iconfont.css";
 
-const { Header } = Layout;
-const { columns1, mappingData1, sourceData1, targetData1 } = SingleNoHeaderData;
-const { columns2, mappingData2, sourceData2, targetData2 } =
-  SingleWithHeaderData;
-const { columns3, mappingData3, sourceData3, targetData3 } = MutiplyMappingData;
-const { columns4, mappingData4, sourceData4, targetData4 } = SinglePointLimit;
-const { sourceColumns, targetColumns, mappingData5, sourceData5, targetData5 } =
-  DiffColumns;
+const columns3 = [
+  {
+    key: "id",
+    width: 30,
+    primaryKey: true,
+  },
+  {
+    key: "name",
+  },
+  {
+    key: "desc",
+    width: 90,
+  },
+];
+
+const sourceData3 = [
+  {
+    id: "source1",
+    title: "来源列1",
+    fields: [
+      {
+        id: "1",
+        name: "性别",
+        desc: "gender",
+      },
+      {
+        id: "2",
+        name: "年龄",
+        desc: "age",
+      },
+      {
+        id: "3",
+        name: "喜好",
+        desc: "hobby",
+      },
+      {
+        id: "4",
+        name: "身高",
+        desc: "height",
+      },
+      {
+        id: "5",
+        name: "体重",
+        desc: "weight",
+      },
+      {
+        id: "6",
+        name: "国籍",
+        desc: "nationality",
+      },
+    ],
+  },
+  {
+    id: "source2",
+    title: "来源列2",
+    fields: [
+      {
+        id: "1",
+        name: "性别",
+        desc: "gender",
+      },
+      {
+        id: "2",
+        name: "年龄",
+        desc: "age",
+      },
+      {
+        id: "3",
+        name: "喜好",
+        desc: "hobby",
+      },
+      {
+        id: "4",
+        name: "身高",
+        desc: "height",
+      },
+      {
+        id: "5",
+        name: "体重",
+        desc: "weight",
+      },
+      {
+        id: "6",
+        name: "国籍",
+        desc: "nationality",
+      },
+    ],
+  },
+];
+
+const targetData3 = [
+  {
+    id: "target1",
+    title: "目标列1",
+    fields: [
+      {
+        id: "1",
+        name: "字段1",
+        desc: "filed1",
+      },
+      {
+        id: "2",
+        name: "字段2",
+        desc: "filed2",
+      },
+      {
+        id: "3",
+        name: "字段3",
+        desc: "filed3",
+      },
+      {
+        id: "4",
+        name: "字段4",
+        desc: "filed4",
+      },
+      {
+        id: "5",
+        name: "字段5",
+        desc: "filed5",
+      },
+      {
+        id: "6",
+        name: "字段6",
+        desc: "filed6",
+      },
+      {
+        id: "7",
+        name: "字段7",
+        desc: "filed7",
+      },
+      {
+        id: "8",
+        name: "字段8",
+        desc: "filed8",
+      },
+    ],
+  },
+  {
+    id: "target2",
+    title: "目标列2(空状态)",
+    fields: [
+      {
+        id: "1",
+        name: "字段1",
+        desc: "filed1",
+      },
+      {
+        id: "2",
+        name: "字段2",
+        desc: "filed2",
+      },
+    ],
+  },
+];
+
+const mappingData3 = [
+  {
+    source: "1",
+    target: "3",
+    sourceNode: "source1",
+    targetNode: "target1",
+  },
+  {
+    source: "2",
+    target: "4",
+    sourceNode: "source1",
+    targetNode: "target1",
+  },
+  {
+    source: "4",
+    target: "1",
+    sourceNode: "source1",
+    targetNode: "target1",
+  },
+];
 
 class Com extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sourceData1,
-      targetData1,
-      mappingData1,
-    };
-  }
-  componentDidMount() {
-    this.setState({
-      sourceData1: _.cloneDeep(sourceData1),
-      targetData1: _.cloneDeep(targetData1),
-      mappingData1: _.cloneDeep(mappingData1),
-    });
-    // setTimeout(() => {
-    //   let _sourceData1 = _.cloneDeep(this.state.sourceData1);
-    //   _sourceData1.fields[4].disable = true;
-    //   let _targetData1 = _.cloneDeep(this.state.targetData1);
-    //   _targetData1.fields[5].disable = true;
-    //   _targetData1.fields[6].disable = true;
-    //   _targetData1.fields[7].disable = true;
-    //   this.setState({
-    //     sourceData1: _sourceData1,
-    //     targetData1: _targetData1
-    //   });
-    // }, 5000);
-  }
   render() {
     return (
-      <Row gutter={[16, 16]}>
-        <Col flex={"600px"}>
-          <DataMapping
-            className="container single-no-header"
-            sourceClassName="single-no-header-source"
-            targetClassName="single-no-header-target"
-            columns={columns1}
-            sourceData={this.state.sourceData1}
-            targetData={this.state.targetData1}
-            config={{
-              checkable: {
-                source: true,
-                target: false,
-              },
-            }}
-            onChange={(data) => {
-              this.setState({
-                sourceData1: _.cloneDeep(data.sourceData),
-                targetData1: _.cloneDeep(data.targetData),
-                mappingData1: _.cloneDeep(data.mappingData),
-              });
-            }}
-            // sourceData={this.state.sourceData1}
-            // targetData={this.state.targetData1}
-            // mappingData={this.state.mappingData1}
-            mappingData={this.state.mappingData1}
-            width={600}
-            height={600}
-            onEdgeClick={(data) => {
-              console.log(data);
-            }}
-          />
-        </Col>
-        <Col flex={"600px"}>
-          <DataMapping
-            className="container single-with-header"
-            columns={columns2}
-            sourceData={sourceData2}
-            targetData={targetData2}
-            mappingData={mappingData2}
-            width={600}
-            height={600}
-          />
-        </Col>
-        <Col flex={"600px"}>
-          <DataMapping
-            className="container mutiply-mapping"
-            columns={columns3}
-            sourceData={sourceData3}
-            targetData={targetData3}
-            mappingData={mappingData3}
-            type={"mutiply"}
-            width={600}
-            height={600}
-            config={{
-              extraPos: {
-                paddingLeft: 10,
-                paddingRight: 10,
-                paddingTop: 10,
-                paddingBottom: 10,
-                paddingCenter: 130,
-              },
-            }}
-          />
-        </Col>
-        <Col flex={"600px"}>
-          <DataMapping
-            className="container mutiply-mapping"
-            columns={columns3}
-            sourceData={sourceData3}
-            targetData={targetData3}
-            mappingData={mappingData3}
-            type={"mutiply"}
-            emptyContent={
-              <div className="empty-content">
-                <p className="desc">暂无数据</p>
-                <p
-                  className="add-field"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    console.log("自定义空状态");
-                  }}
-                >
-                  + 添加字段
-                </p>
-              </div>
-            }
-            width={600}
-            height={600}
-            config={{
-              sortable: true,
-              checkable: {
-                source: true,
-                target: false,
-              },
-              extraPos: {
-                paddingLeft: 10,
-                paddingRight: 10,
-                paddingTop: 10,
-                paddingBottom: 10,
-                paddingCenter: 130,
-              },
-            }}
-          />
-        </Col>
-        <Col flex={"600px"}>
-          <DataMapping
-            className="container single-no-header"
-            columns={columns4}
-            sourceData={sourceData4}
-            targetData={targetData4}
-            mappingData={mappingData4}
-            width={600}
-            height={600}
-            config={{
-              linkNumLimit: {
-                source: 2,
-                target: 1,
-              },
-              extraPos: {
-                paddingLeft: 10,
-                paddingRight: 10,
-                paddingTop: 10,
-                paddingBottom: 10,
-                paddingCenter: 100,
-              },
-            }}
-          />
-        </Col>
-        <Col flex={"600px"}>
-          <DataMapping
-            className="container single-no-header"
-            sourceClassName="single-no-header-source"
-            targetClassName="single-no-header-target"
-            sourceColumns={sourceColumns}
-            targetColumns={targetColumns}
-            sourceData={sourceData5}
-            targetData={targetData5}
-            mappingData={mappingData5}
-            width={600}
-            height={600}
-          />
-        </Col>
-      </Row>
+      <DataMapping
+        columns={columns3}
+        sourceData={sourceData3}
+        targetData={targetData3}
+        mappingData={mappingData3}
+        type={"mutiply"}
+        width={600}
+        height={600}
+      />
     );
   }
 }
 
-ReactDOM.render(
-  <Router>
-    <Layout>
-      <Header className="header">DTDesign-React数据映射组件</Header>
-      <Layout>
-        <Com />
-      </Layout>
-    </Layout>
-  </Router>,
-  document.getElementById("main")
-);
+ReactDOM.render(<Com />, document.getElementById("main"));
